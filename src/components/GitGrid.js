6de6dd49b5f34b74a2c55dfc300b2129fd3ff7d8
aroldo.acevedo <aroldoacevedo@gmail.com>
2021-01-16 18:39:53 -0300

    Se crea un Custom Hook

--- src/components/GitGrid.js
+++ src/components/GitGrid.js
@@ -1,39 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+//import { getGifs } from '../helpers/getGifs';
 
 const GitGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-    //const [value, setValue] = useState(0);
-    //Renderizado por primera vez
-    useEffect(() => {
-        getGifs( category )
-        .then( setImages );
-        //.then( imgs => setImages(imgs));
-    },[ category ]);
+    //const [images, setImages] = useState([]);
+    const { data: images, loading } = useFetchGifs(category);
 
     return (<>
-                <h3>{ category }</h3>  
-                <div className="card-grid">
+                <h3 className ="animate__animated animate__fadeIn">{ category }</h3>  
+                {loading && <p className="animate__animated animate__flash">Loading</p>}
+                {<div className="card-grid">
                     {
                         images.map(img => (
                             <GifGridItem 
                             key={img.id}
                             { ...img}
-                            /*img={img} */
                             />
                         ))
-
-                        /*images.map(img => (
-                            <li key ={img.id}>{img.title}</li>
-                        ))*/
                     }
                 
-                    {/*<h2>{count}</h2> 
-                    <button onClick= {() => setCount(count + 1) }></button> */}
-                
-                </div>
+                </div>}
             </>
     )
 }
